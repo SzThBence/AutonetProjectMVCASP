21d0d38eda3b66f69de759977cdad63478c4e0b6
SzThBence IWB0TM <szthbence@gmail.com>
2023-07-05 15:02:23 +0200

    Added Appointments and Employees tables

---
+++ AutonetProjectMVCASP/Models/Appointments.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutonetProjectMVCASP.Models
+{
+    public class Appointments
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime Time { get; set; }
+        public string? Location { get; set; }
+    }
+}
---
+++ AutonetProjectMVCASP/Models/Employees.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutonetProjectMVCASP.Models
+{
+    public class Employees
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Job { get; set; }
+    }
+}
