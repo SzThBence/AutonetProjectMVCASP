a3482dff0fcd29b3ba377d806bbd518a8bd0e5d4
SzThBence IWB0TM <szthbence@gmail.com>
2023-07-05 14:20:42 +0200

    Add project files.

---
+++ AutonetProjectMVCASP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AutonetProjectMVCASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
