02d8a0f7c619ebfbc80465731dd1101e69f2c605
SzThBence IWB0TM <szthbence@gmail.com>
2023-07-05 15:02:44 +0200

    additional modifications

--- AutonetProjectMVCASP/Models/Appointments.cs
+++ AutonetProjectMVCASP/Models/Appointments.cs
@@ -6,6 +6,7 @@ namespace AutonetProjectMVCASP.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string? Name { get; set; }
         public DateTime Time { get; set; }
         public string? Location { get; set; }
