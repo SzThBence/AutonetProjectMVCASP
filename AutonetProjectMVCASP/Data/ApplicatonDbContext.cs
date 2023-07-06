435def15b703794b87b27bfa68b295bf9a6bcd06
SzThBence IWB0TM <szthbence@gmail.com>
2023-07-06 08:04:28 +0200

    Added some setup stuff, default path is ., that will need to be changed

---
+++ AutonetProjectMVCASP/Data/ApplicatonDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AutonetProjectMVCASP.Models;
+
+namespace TutorialWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Category> Category { get; set; }
+    }
+}
