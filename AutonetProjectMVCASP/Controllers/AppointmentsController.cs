f3f74d8c31a04b7bfe5b8c346909dba23f72350a
SzThBence IWB0TM <szthbence@gmail.com>
2023-07-06 12:09:56 +0200

    Added table to Appointments, made it display, if a date is occupied

--- AutonetProjectMVCASP/Controllers/AppointmentsController.cs
+++ AutonetProjectMVCASP/Controllers/AppointmentsController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutonetProjectMVCASP.Data;
+using Microsoft.AspNetCore.Mvc;
+using AutonetProjectMVCASP.Models;
 
 namespace AutonetProjectMVCASP.Controllers
 {
     public class AppointmentsController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public AppointmentsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Appointments> obj = _db.Appointments;
+            return View(obj);
         }
 
         public IActionResult Appointments()
