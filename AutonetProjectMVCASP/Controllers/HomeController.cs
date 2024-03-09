9a24140f3f4e4a6c34dbad91c662a4819d2cf262
Bence Szabó-Thalmeiner <iwb0tm@INF.ELTE.HU>
2024-03-09 17:46:29 +0200

    Added a page to manage roles, solved role checking, now it syncs up instantly with the database

--- AutonetProjectMVCASP/Controllers/HomeController.cs
+++ AutonetProjectMVCASP/Controllers/HomeController.cs
@@ -6,10 +6,27 @@ using NToastNotify;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
 
 
 namespace AutonetProjectMVCASP.Controllers
 {
+    public class UsersAndRolesManagers
+    {
+        public UserManager<IdentityUser> _userManager;
+        public RoleManager<IdentityRole> _roleManager;
+
+
+        public UsersAndRolesManagers(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+    }
+
     public class HomeController : Controller
     {
 
@@ -18,20 +35,56 @@ namespace AutonetProjectMVCASP.Controllers
         private readonly INotyfService _toastNotification;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
 
 
-        public HomeController(ApplicationDbContext db, ILogger<HomeController> logger, INotyfService toastNotification, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        public HomeController(ApplicationDbContext db, ILogger<HomeController> logger, INotyfService toastNotification, UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, SignInManager<IdentityUser> signInManager)
         {
             _db = db;
             _logger = logger;
             _toastNotification = toastNotification;
             _userManager = userManager;
             _roleManager = roleManager;
+            _signInManager = signInManager;
         }
 
         [HttpGet]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            await AddRolesAndUsers();
+
+            //// Sign out the current user
+            //await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            //// Authenticate the user again (example: using a predefined user)
+            //var user = new ClaimsPrincipal(new ClaimsIdentity(new[]
+            //{
+            //    new Claim(ClaimTypes.Name, "username"),
+            //    // Add other claims as needed
+            //}, CookieAuthenticationDefaults.AuthenticationScheme));
+
+            //// Sign in the user again
+            //await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, user);
+
+            // Example of signing out
+            await HttpContext.SignOutAsync();
+            if (_signInManager.IsSignedIn(User))
+            {
+                // Example of signing in again
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                var claims = await _userManager.GetClaimsAsync(user); // You may need to retrieve claims if necessary
+
+                // Create a ClaimsIdentity for the user
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                // Create a ClaimsPrincipal using the ClaimsIdentity
+                var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+
+                // Sign in the user
+                await HttpContext.SignInAsync(claimsPrincipal);
+            }
+            
+
             return View();
         }
         [HttpGet]
@@ -42,10 +95,41 @@ namespace AutonetProjectMVCASP.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Users()
+        public IActionResult Users()
         {
-            var users = await _userManager.Users.ToListAsync();
-            return View(users);
+            UsersAndRolesManagers usersAndRoles = new UsersAndRolesManagers(_userManager, _roleManager);
+            return View(usersAndRoles);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RoleChanger(Dictionary<string, List<string>> roles)
+        {
+            if (roles != null)
+            {
+                foreach (var userId in roles.Keys)
+                {
+                    var user = await _userManager.FindByIdAsync(userId);
+                    if (user != null)
+                    {
+                        // Clear existing roles for the user
+                        var userRoles = await _userManager.GetRolesAsync(user);
+                        await _userManager.RemoveFromRolesAsync(user, userRoles);
+
+                        // Add selected roles for the user
+                        foreach (var roleName in roles[userId])
+                        {
+                            if (await _roleManager.RoleExistsAsync(roleName))
+                            {
+                                await _userManager.AddToRoleAsync(user, roleName);
+                            }
+                        }
+                    }
+                    await _userManager.UpdateAsync(user);
+                }
+            }
+
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -55,7 +139,7 @@ namespace AutonetProjectMVCASP.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> AddRolesAndUsers()
+        public async Task<Boolean> AddRolesAndUsers()
         {
             // Create roles if they don't exist
             var roles = new List<string> { "Admin", "User", "Boss", "Architect" };
@@ -83,7 +167,7 @@ namespace AutonetProjectMVCASP.Controllers
                 await _userManager.AddToRoleAsync(adminUser, "Admin");
             }
 
-            return RedirectToAction("Index"); // Redirect to a specific action after completing the role assignment
+            return true;
         }
     }
 }
