d3385e03dbda8f9c79767c7c9e6e4df6f8795120
Bence Szabó-Thalmeiner <iwb0tm@INF.ELTE.HU>
2024-03-26 14:04:48 +0200

    Simple additions, updates

--- AutonetProjectMVCASP/Data/ApplicatonDbContext.cs
+++ AutonetProjectMVCASP/Data/ApplicatonDbContext.cs
@@ -8,6 +8,7 @@ namespace AutonetProjectMVCASP.Data
     public class ApplicationDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public ApplicationDbContext() : base() { }
         public virtual DbSet<Appointments> Appointments { get; set; }
         public virtual DbSet<Employees> Employees { get; set; }
         public virtual DbSet<Locations> Locations { get; set; }
