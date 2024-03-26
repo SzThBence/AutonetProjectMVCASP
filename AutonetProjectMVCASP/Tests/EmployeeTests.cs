c3373c1f61b1bfbc7b127482069178ba3bdd42b9
Bence Szabó-Thalmeiner <iwb0tm@INF.ELTE.HU>
2024-03-26 14:31:42 +0200

    Basic tests, If Microsoft.NET.Test.Sdk is on, the page won't start, if it is off, the tests won't.

---
+++ AutonetProjectMVCASP/Tests/EmployeeTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutonetProjectMVCASP.Tests
+{
+    
+
+    [TestClass]
+    public class EmployeeTests
+    {
+        private AutonetProjectMVCASP.Models.Employees employee;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            employee = new AutonetProjectMVCASP.Models.Employees();
+        }
+
+        [TestMethod]
+        public void TestEmployeeName()
+        {
+
+            // Act
+            employee.Name = "John";
+
+            // Assert
+            Assert.AreEqual("John", employee.Name);
+        }
+
+        [TestMethod]
+        public void TestEmployeeSurname()
+        {
+            
+
+            // Act
+            employee.Surname = "Doe";
+
+            // Assert
+            Assert.AreEqual("Doe", employee.Surname);
+        }
+
+        [TestMethod]
+        public void TestEmployeeJob()
+        {
+            
+
+            // Act
+            employee.Job = "Manager";
+
+            // Assert
+            Assert.AreEqual("Manager", employee.Job);
+        }
+
+        [TestMethod]
+        public void TestEmployeeImagePath()
+        {
+            
+
+            // Act
+            employee.ImagePath = "images/john_doe.jpg";
+
+            // Assert
+            Assert.AreEqual("images/john_doe.jpg", employee.ImagePath);
+
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            // Cleanup code, if any
+        }
+    }
+}
