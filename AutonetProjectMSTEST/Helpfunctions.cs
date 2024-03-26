41b9192d9aa5a63f3c206b24472f0c2f0033537d
Bence Szabó-Thalmeiner <iwb0tm@INF.ELTE.HU>
2024-03-26 13:57:46 +0200

    Added testing project, weird bug, where it won't start

---
+++ AutonetProjectMSTEST/Helpfunctions.cs
@@ -0,0 +1,25 @@
+﻿using AutonetProjectMVCASP.Data;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutonetProjectMSTEST
+{
+    public class HelpFunctions<T> where T : class
+    {
+        public static DbSet<T> ReturnsDbSet<T>(Mock<ApplicationDbContext> mock, IQueryable<T> data) where T : class
+        {
+            var mockSet = new Mock<DbSet<T>>();
+            mockSet.As<IQueryable<T>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<T>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+            mock.Setup(db => db.Set<T>()).Returns(mockSet.Object);
+            return mockSet.Object;
+        }
+    }
+}
--- AutonetProjectMVCASP/Data/ApplicatonDbContext.cs
+++ AutonetProjectMVCASP/Data/ApplicatonDbContext.cs
@@ -8,6 +8,7 @@ namespace AutonetProjectMVCASP.Data
     public class ApplicationDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public ApplicationDbContext() { }
         public virtual DbSet<Appointments> Appointments { get; set; }
         public virtual DbSet<Employees> Employees { get; set; }
         public virtual DbSet<Locations> Locations { get; set; }
