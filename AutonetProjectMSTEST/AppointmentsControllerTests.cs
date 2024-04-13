bd5627bcffa3894fd2dd8bc23cb8f02d2072214c
Bence Szabó-Thalmeiner <iwb0tm@INF.ELTE.HU>
2024-04-13 14:12:32 +0200

    Added base testing classes for all controllers, now we just need the functions

---
+++ AutonetProjectMSTEST/AppointmentsControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using AutonetProjectMVCASP.Controllers;
+using AutonetProjectMVCASP.Data;
+using AutonetProjectMVCASP.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AutonetProjectMSTEST;
+
+namespace AutonetProjectMSTEST
+{
+    [TestClass]
+    public class AppointmentsControllerTests
+    {
+        private AppointmentsController _controller;
+        private Mock<ApplicationDbContext> _mockDb;
+        private Mock<ILogger<AppointmentsController>> _mockLogger;
+        private Mock<INotyfService> _mockToastNotification;
+        private Mock<IWebHostEnvironment> _mockHostingEnvironment;
+    }
+}
